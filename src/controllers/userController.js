d94cd4999794fa835159d35979cc947b4e0e4b21
Santiago Toro <santiagotoro4621@gmail.com>
2025-03-08 12:36:51 -0500

    Se agrega await para manejar la función asincrona

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,7 +4,7 @@ const User = require('../models/userModel.js');
 
 const getUsers = async (req, res) => {
   try {
-    const users =  User.find();
+    const users = await User.find();
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: 'Server error' });
